bc8d5a3a814ab555c9a879c2db33376bb85cd836
MaghaliMarvelous <133017983+MaghaliMarvelous@users.noreply.github.com>
2025-09-08 23:34:40 +0700

    add some components

---
+++ lib/Components/input_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class InputField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final IconData icon;
+  final bool obscure;
+
+  const InputField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.icon,
+    this.obscure = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obscure,
+      decoration: InputDecoration(
+        prefixIcon: Icon(icon),
+        hintText: hintText,
+        filled: true,
+        fillColor: Colors.grey[200],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+}
