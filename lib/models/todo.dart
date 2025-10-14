67b4a361623350416ca4eed95cce3462a862ce3a
MaghaliMarvelous <133017983+MaghaliMarvelous@users.noreply.github.com>
2025-10-14 09:37:20 +0700

    Database Update

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,13 +1,19 @@
 class Todo {
+  int? id;
   String title;
   String description;
   String category;
   bool isDone;
+  String? startTime;
+  String? endTime;
 
   Todo({
+    this.id,
     required this.title,
     required this.description,
     required this.category,
     this.isDone = false,
+    this.startTime,
+    this.endTime,
   });
 }
