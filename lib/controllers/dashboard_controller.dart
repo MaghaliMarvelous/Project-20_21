4fd20c2f034ca0dac554bcc7d95454a50a6c9932
MaghaliMarvelous <133017983+MaghaliMarvelous@users.noreply.github.com>
2025-10-14 10:22:59 +0700

    DB fix

--- lib/controllers/dashboard_controller.dart
+++ lib/controllers/dashboard_controller.dart
@@ -1,30 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../models/todo.dart';
+import '../helper/db_helper.dart';
 import 'history_controller.dart';
 
 class DashboardController extends GetxController {
+  var isMobile = true.obs;
+  var todos = <Todo>[].obs;
 
-   var isMobile = true.obs;
+  final DBHelper dbHelper = DBHelper();
 
   void updateLayout(BoxConstraints constraints) {
     isMobile.value = constraints.maxWidth < 600;
   }
-  var todos = <Todo>[].obs;
 
-  void addTodo(Todo todo) => todos.add(todo);
-
-  void editTodo(int index, Todo updatedTodo) => todos[index] = updatedTodo;
-
-  void toggleDone(int index) {
-    todos[index].isDone = !todos[index].isDone;
-    todos.refresh();
+  @override
+  void onInit() {
+    super.onInit();
+    loadTodos(); // 🔹 ambil data dari database saat aplikasi dimulai
   }
 
-  void delete(int index) {
-    final deleted = todos.removeAt(index);
+  Future<void> loadTodos() async {
+    final list = await dbHelper.getTodos();
+    todos.assignAll(list);
+  }
+
+  Future<void> addTodo(Todo todo) async {
+    await dbHelper.insertTodo(todo);
+    await loadTodos(); 
+  }
+
+  Future<void> editTodo(int index, Todo updatedTodo) async {
+    await dbHelper.updateTodo(updatedTodo);
+    await loadTodos();
+  }
+
+  Future<void> toggleDone(int index) async {
+    final todo = todos[index];
+    final newStatus = !todo.isDone;
+    await dbHelper.updateTodoDoneStatus(todo.id!, newStatus);
+    todo.isDone = newStatus;
+    todos[index] = todo;
+  }
+
+  Future<void> delete(int index) async {
+    final todo = todos[index];
+    await dbHelper.deleteTodo(todo.id!);
+    todos.removeAt(index);
+
+    // 🔹 simpan ke history (jika controller ada)
     if (Get.isRegistered<HistoryController>()) {
-      Get.find<HistoryController>().addToHistory(deleted);
+      Get.find<HistoryController>().addToHistory(todo);
     }
   }
 }
